5e42f41957b99fd94b55a82ea62b75ecfe4876dd
Joseph Ampadu <jkampadujnr@gmail.com>
2023-04-18 08:51:34 +0000

    Add columns for db storage Place

--- models/city.py
+++ models/city.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+
 from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
 
 from models.base_model import Base, BaseModel
 
@@ -12,3 +14,4 @@ class City(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", cascade="all", backref="cities")
--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -14,3 +15,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = relationship("Place", cascade="all", backref="user")
