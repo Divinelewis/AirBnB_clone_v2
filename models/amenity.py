756b8673e05092128de6cafafea870a78c7485c8
Divinelewis <divinitysoft0@gmail.com>
2023-04-19 19:33:49 +0100

    my git commit

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-
+from models.base_model import BaseModel, Base
+from models import storage_type
 from sqlalchemy import Column, String
-
-from models.base_model import Base, BaseModel
 
 
 class Amenity(BaseModel, Base):
-    """Defines an amenity"""
-
-    __tablename__ = "amenities"
-
-    name = Column(String(128), nullable=False)
+    '''amenity class'''
+    __tablename__ = 'amenities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
