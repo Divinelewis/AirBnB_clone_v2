118be19f618b347da9da9a867843eaab77950750
Divinelewis <divinitysoft0@gmail.com>
2023-04-19 11:24:20 +0100

    my git commit

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -2,10 +2,11 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.place import Place
+import os
 
 
 class test_Place(test_basemodel):
-    """ """
+    """test for city"""
 
     def __init__(self, *args, **kwargs):
         """ """
