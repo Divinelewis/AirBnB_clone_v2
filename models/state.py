17c2d2e69e43ee00b12677b80f2a4c9b7994e472
Divinelewis <divinitysoft0@gmail.com>
2023-04-19 19:35:17 +0100

    state commit

--- models/state.py
+++ models/state.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-
+from models.base_model import BaseModel, Base
+from models import storage_type
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-from models.base_model import Base, BaseModel
-from models.city import City
-import models
-
 
 class State(BaseModel, Base):
-    """ State class """
+    """ State class / table model"""
+    __tablename__ = 'states'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
+    else:
+        name = ''
 
-    __tablename__ = "states"
-
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all", backref="state")
-
-    @property
-    def cities(self):
-        """Getter attribute - cities"""
-        state_cities = []
-        for city in models.storage.all(City).values():
-            if city.state_id == self.id:
-                state_cities.append(city)
-        return state_cities
+        @property
+        def cities(self):
+            '''returns the list of City instances with state_id
+                equals the current State.id
+                FileStorage relationship between State and City
+            '''
+            from models import storage
+            related_cities = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    related_cities.append(city)
+            return related_cities
