c351a777637c32034f547caf92cc17cc8b8d7d09
Divinelewis <divinitysoft0@gmail.com>
2023-05-20 18:58:56 +0100

    changes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """Script that starts a flask web application
 Web app must be listening at 0.0.0.0, port 5000
-Routes / display "Hello HBNB"
-       /hbnb: display "HBNB"
+Routes / - display "Hello HBNB"
+       /hbnb - display "HBNB"
 """
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-def route_hbnb():
+def hbnb_route():
     """ Prints Hello HBNB!"""
     return "Hello HBNB!"
 
@@ -19,4 +19,4 @@ def hbnb():
     return "HBNB"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
